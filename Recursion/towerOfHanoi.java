c04b22402754ba8652427ed0b08585b6f91d5f17
Thunder <74486623+ThunderE75@users.noreply.github.com>
2024-01-14 15:52:42 +0530

    Tower Of Hanoi visual fix

--- Recursion/towerOfHanoi.java
+++ Recursion/towerOfHanoi.java
@@ -1,26 +1,29 @@
 package Recursion;
+
 import java.util.*;
-import java.io.*;
-import java.math.*;
 
 class towerOfHanoi {
-    static int moves=0;
+    static int moves = 0;
+
     static void towerofHanoi(int n, char from_peg, char to_peg, char aux_peg) {
         if (n == 1) {
-            System.out.println((++moves) +" - Move disk 1 from peg " + from_peg + " to peg " + to_peg);
+            System.out.println((++moves) + " - Move disk 1 from peg " + from_peg + " to peg " + to_peg);
             return;
         }
         towerofHanoi(n - 1, from_peg, aux_peg, to_peg);
-        System.out.println((++moves)+" - Move disk " + n + " from peg " + from_peg + " to peg " + to_peg);
+        System.out.println((++moves) + " - Move disk " + n + " from peg " + from_peg + " to peg " + to_peg);
         towerofHanoi(n - 1, aux_peg, to_peg, from_peg);
     }
 
-    // Driver code
-
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter no. of disks: ");
-        int n = sc.nextInt();; // number of disks
+        System.out.print("\033[H\033[2J"); // Cleans the terminal output
+        System.out.print("# Enter the number of disks: ");
+        int n = sc.nextInt(); // number of disks
+        System.out.println("====================================");
         towerofHanoi(n, 'A', 'C', 'B'); // A , B and C are names of peg
+        System.out.println("====================================");
+        System.out.println("> " + n + " disks took " + moves + " steps.");
+        sc.close();
     }
 }
